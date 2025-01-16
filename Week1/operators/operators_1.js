1d895f33fbdb163cfd2c2c8497b15517203fbc31
Jay-wilco <jtvilkko@gmail.com>
2025-01-16 14:54:55 +0200

    end of lesson commit

---
+++ Week1/operators/operators_1.js
@@ -0,0 +1,32 @@
+/* 
+1. Create two variables, a and b, and assign them integer values (e.g., 8 and 3).
+2. Perform the following operations using a and b, and store the results in separate variables:
+ - Add the values (sum).
+ - Subtract b from a (difference).
+ - Multiply the values (product).
+ - Divide a by b (quotient).
+ - Find the remainder when a is divided by b (remainder).
+3. Use console.log() to print the results of each operation. For example, print: "The sum of a and b is: 11"
+4. Add comments to explain what each operation does.
+5. Change the values of a and b and observe how the results change.
+6. Try the same operations with one positive and one negative number.
+*/
+
+let a = 8;
+let b = 3;
+// these are the variables
+let sum = a + b;
+
+console.log("Sum of a and b is:", sum);
+//this prints the sum of variables a and b
+let Subtract = a - b;
+console.log("Subtract of a and b is:", Subtract);
+
+let Multiply = a * b;
+console.log("Multiply of a and b is:", Multiply);
+
+let Divide = a / b;
+console.log("Divide of a and b is:", Divide);
+
+let remainder = a % b;
+console.log("Remainder of a and b is", remainder);
